994bfc1b6aa78db6f4afd824ffd5489b59dd8e81
kelemengqi <1565916105@qq.com>
2024-10-25 23:29:48 +0800

    15.3
    15.3

--- myhomework/src/views/EventListView.vue
+++ myhomework/src/views/EventListView.vue
@@ -2,7 +2,7 @@
  * @Author: kelemengqi 1565916105@qq.com
  * @Date: 2024-10-24 23:37:05
  * @LastEditors: kelemengqi 1565916105@qq.com
- * @LastEditTime: 2024-10-25 19:14:49
+ * @LastEditTime: 2024-10-25 23:26:05
  * @FilePath: /vite-class-lab2/myhomework/src/views/EventListView.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,43 +10,22 @@
 import EventCard from '@/components/EventCard.vue'
 import EventInfo from '@/components/EventInfo.vue'
 import type { Event } from '@/type'
-import { ref } from 'vue'
+import { ref,onMounted } from 'vue'
 
-const events = ref<Event[]>([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28, 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee'
-  },
-  {
-    id: 4582797,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community edible plants.',
-    location: 'Flora City',
-    date: 'March 14, 2022',
-    time: '10:00',
-    petsAllowed: true,
-    organizer: 'Fern Pollin'
-  },
-  {
-    id: 8419988,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22, 2022',
-    time: '11:00',
-    petsAllowed: false,
-    organizer: 'Carey Wales'
-  }
-])
+import axios from 'axios'
+
+ const events = ref<Event[]>(null)
+
+onMounted(() => {
+ axios
+    .get('[your mock server url]')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
 </script>
 
 <template>
