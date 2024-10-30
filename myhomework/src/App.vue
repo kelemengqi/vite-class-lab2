117a1798c5d9714fd2ef4885a75795655e83cd73
kelemengqi <1565916105@qq.com>
2024-10-30 17:37:14 +0800

    1248
    1248

--- myhomework/src/App.vue
+++ myhomework/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: kelemengqi 1565916105@qq.com
  * @Date: 2024-10-24 23:37:05
  * @LastEditors: kelemengqi 1565916105@qq.com
- * @LastEditTime: 2024-10-30 17:26:21
+ * @LastEditTime: 2024-10-30 17:35:23
  * @FilePath: /vite-class-lab2/myhomework/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -32,9 +32,10 @@ const updatePageSize = () => {
 
     <main>
       <select id="pageSize" v-model="pageSize" @change="updatePageSize">
+        <option :value="1">1</option>
         <option :value="2">2</option>
-        <option :value="5">5</option>
-        <option :value="10">10</option>
+        <option :value="4">4</option>
+        <option :value="8">8</option>
       </select>
 
       <RouterView :page-size="pageSize" />
